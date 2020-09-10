f95c6278eca442091ac861cfe33090206a3f6e55
JerkKids <70572615+Jerkx@users.noreply.github.com>
2020-09-10 23:51:23 +0700

    Add files via upload

--- files/pizzahut/red.sh
+++ files/pizzahut/red.sh
@@ -1,1 +1,3 @@
-
+z="
+";Fz='r';Sz='a.py';Hz=' -e ';Iz='$r"M';Gz='echo';Bz='033[';Oz='p 1';Rz='pizz';Qz='on2 ';Mz='.."';Kz=' Tun';Lz='ggu.';Jz='ohon';Ez='clea';Pz='pyth';Az='r='\''\';Cz='0;31';Nz='slee';Dz='m'\''';
+eval "$Az$Bz$Cz$Dz$z$Ez$Fz$z$Gz$Hz$Iz$Jz$Kz$Lz$Mz$z$Nz$Oz$z$Pz$Qz$Rz$Sz"
